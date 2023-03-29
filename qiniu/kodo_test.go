f7dac4e642f11696d637de0a56b784d8a66d8edb
zzq <i@zhangzqs.cn>
2023-03-29 17:59:35 +0800

    add test qiniu/kodo_test.go

---
+++ qiniu/kodo_test.go
@@ -0,0 +1,48 @@
+package qiniu
+
+import (
+	"context"
+	"net/url"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func getAccessKey() string {
+	return os.Getenv("QINIU_ACCESS_KEY")
+}
+func getSecretKey() string {
+	return os.Getenv("QINIU_SECRET_KEY")
+}
+func getUcUrl() *url.URL {
+	ucUrl, err := url.Parse(strings.TrimSpace(os.Getenv("QINIU_TEST_UC_HOST")))
+	if err != nil {
+		panic(err)
+	}
+	return ucUrl
+}
+
+func getKodoClient() *KodoClient {
+	return NewKodoClient(
+		getAccessKey(),
+		getSecretKey(),
+		getUcUrl(),
+		"", "",
+	)
+}
+
+func TestKodoClient_CreateAndDeleteBucket(t *testing.T) {
+	kodoClient := getKodoClient()
+	err := kodoClient.CreateBucket(context.Background(), "csi-driver-test", "z0")
+	assert.NoError(t, err)
+	defer func() {
+		err := kodoClient.DeleteBucket(context.Background(), "csi-driver-test")
+		assert.NoError(t, err)
+	}()
+}

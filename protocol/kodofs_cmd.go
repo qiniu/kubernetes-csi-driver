220f5eef25d32545d10c2538f4492eff81d112f7
zzq <i@zhangzqs.cn>
2023-05-26 17:43:31 +0800

    fix DeleteVolume error

--- protocol/kodofs_cmd.go
+++ protocol/kodofs_cmd.go
@@ -7,15 +7,20 @@ import (
 )
 
 type InitKodoFSMountCmd struct {
-	VolumeId  string `json:"volume_id"`
-	GatewayID string `json:"gateway_id"`
-	MountPath string `json:"mount_path"`
-	SubDir    string `json:"sub_dir"`
+	VolumeId     string `json:"volume_id"`
+	GatewayID    string `json:"gateway_id"`
+	MountPath    string `json:"mount_path"`
+	SubDir       string `json:"sub_dir"`
+	RunOnSystemd bool   `json:"run_on_systemd"`
 }
 
 func (*InitKodoFSMountCmd) Command() {}
 
 func (c *InitKodoFSMountCmd) ExecCommand(ctx context.Context) *exec.Cmd {
 	var args = []string{"mount", c.GatewayID, c.MountPath, "-s", c.SubDir, "--force_reinit"}
-	return execOnSystemd(ctx, fmt.Sprintf("run-kodofs-%s.service", c.VolumeId), KodoFSCmd, args...)
+	if c.RunOnSystemd {
+		return execOnSystemd(ctx, fmt.Sprintf("run-kodofs-%s.service", c.VolumeId), KodoFSCmd, args...)
+	} else {
+		return exec.CommandContext(ctx, KodoFSCmd, args...)
+	}
 }
